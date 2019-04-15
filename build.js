8a92624ceb5593c79526f10e4f89b77fc133dba9
Travis Vroman <travisv@mahigaming.com>
2019-04-15 11:37:13 -0400

    Updated build process to use node instead of batch

---
+++ build.js
@@ -0,0 +1,59 @@
+var fs = require("fs");
+var fsExtra = require("fs-extra");
+var cp = require("child_process");
+var path = require("path");
+
+function copyFileSync(source, target) {
+
+    var targetFile = target;
+
+    // If target is a directory a new file with the same name will be created
+    if (fs.existsSync(target)) {
+        if (fs.lstatSync(target).isDirectory()) {
+            targetFile = path.join(target, path.basename(source));
+        }
+    }
+
+    fs.writeFileSync(targetFile, fs.readFileSync(source));
+}
+
+function copyFolderRecursiveSync(source, target) {
+    var files = [];
+
+    // Check if folder needs to be created or integrated
+    var targetFolder = target;//path.join(target, path.basename(source));
+    if (!fs.existsSync(targetFolder)) {
+        fs.mkdirSync(targetFolder);
+    }
+
+    // Copy
+    if (fs.lstatSync(source).isDirectory()) {
+        files = fs.readdirSync(source);
+        files.forEach(function (file) {
+            var curSource = path.join(source, file);
+            if (fs.lstatSync(curSource).isDirectory()) {
+                copyFolderRecursiveSync(curSource, targetFolder);
+            } else {
+                copyFileSync(curSource, targetFolder);
+            }
+        });
+    }
+}
+console.log("\x1b[35m***BUILD***\x1b[0m");
+
+console.log("Cleaning...");
+fsExtra.emptyDirSync("dist");
+
+console.log("Building...");
+cp.execSync("node node_modules/typescript/bin/tsc");
+
+console.log("Copying dependencies...");
+copyFolderRecursiveSync("node_modules/namorvtech/dist/", "dist");
+
+console.log("Copying www...");
+copyFolderRecursiveSync("www/", "dist");
+
+console.log("Copying assets...");
+copyFolderRecursiveSync("assets/", "dist/assets");
+
+console.log("\x1b[35m***BUILD COMPLETE***\x1b[0m");
